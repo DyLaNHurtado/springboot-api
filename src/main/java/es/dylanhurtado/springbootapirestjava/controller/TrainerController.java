e1e4ac49ab449246084112069adccecc9e88afbe
Dylan Hurtado López <dylahurtado43@gmail.com>
2022-02-25 19:15:11 +0100

    mappers repos and doing Restcontrollers

--- src/main/java/es/dylanhurtado/springbootapirestjava/controller/TrainerController.java
+++ src/main/java/es/dylanhurtado/springbootapirestjava/controller/TrainerController.java
@@ -1,14 +1,18 @@
 package es.dylanhurtado.springbootapirestjava.controller;
 
+import es.dylanhurtado.springbootapirestjava.dto.PokemonDTO;
 import es.dylanhurtado.springbootapirestjava.dto.TrainerDTO;
 import es.dylanhurtado.springbootapirestjava.mappers.TrainerMapper;
+import es.dylanhurtado.springbootapirestjava.model.Pokemon;
+import es.dylanhurtado.springbootapirestjava.model.Trainer;
 import es.dylanhurtado.springbootapirestjava.repositories.TrainerRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 
 @RestController
@@ -16,11 +20,40 @@ import java.util.List;
 @RequestMapping("/trainer")
 public class TrainerController {
 
-    private final TrainerRepository trainerRepository;
-    private final TrainerMapper trainerMapper;
+    private final TrainerRepository repository;
+    private final TrainerMapper mapper;
 
-    @GetMapping("/all")
-    public List<TrainerDTO> getAll() {
-        return trainerMapper.toDTO(trainerRepository.findAll());
+    @GetMapping("/")
+    public ResponseEntity<List<TrainerDTO>> getAll() {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .body(mapper.toDTO(repository.findAll()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TrainerDTO> findById(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .body(mapper.toDTO(repository.findById(id).orElseThrow(NullPointerException::new)));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<TrainerDTO> postClient(@RequestBody TrainerDTO trainerDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(mapper.toDTO(repository.save(mapper.toModel(trainerDTO))));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<TrainerDTO> putClient(@RequestBody TrainerDTO trainerDTO) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(mapper.toDTO(repository.save(mapper.toModel(trainerDTO))));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<TrainerDTO> delete(@PathVariable UUID id) {
+        Trainer trainer = repository.findById(id).orElse(null);
+        if (trainer != null) {
+            repository.delete(trainer);
+            return ResponseEntity.ok(mapper.toDTO(trainer));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new TrainerDTO());
     }
 }
